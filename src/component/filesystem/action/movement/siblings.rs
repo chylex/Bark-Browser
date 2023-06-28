843ef4ffe4def04f9915f6f2f7bd43d3f84b406b
chylex <contact@chylex.com>
2023-06-28 03:14:36 +0200

    Make movements to next/previous sibling go up a level when reaching the last sibling

--- src/component/filesystem/action/movement/siblings.rs
+++ src/component/filesystem/action/movement/siblings.rs
@@ -6,8 +6,22 @@ use crate::component::filesystem::tree::{FsTreeView, FsTreeViewNode};
 pub struct MoveToNextSibling;
 
 impl MovementAction for MoveToNextSibling {
-	fn get_target(_tree: &FsTreeView, selected_node: &NodeRef<FsTreeViewNode>) -> Option<NodeId> where Self: Sized {
-		selected_node.next_sibling().map(|sibling| sibling.node_id())
+	fn get_target(tree: &FsTreeView, selected_node: &NodeRef<FsTreeViewNode>) -> Option<NodeId> where Self: Sized {
+		let mut node_id = selected_node.node_id();
+		
+		while let Some(node) = tree.get(node_id) {
+			let sibling_id = node.next_sibling().map(|sibling| sibling.node_id());
+			
+			if sibling_id.is_some() {
+				return sibling_id;
+			} else if let Some(parent) = node.parent() {
+				node_id = parent.node_id();
+			} else {
+				return None;
+			}
+		}
+		
+		None
 	}
 }
 
@@ -15,6 +29,6 @@ pub struct MoveToPreviousSibling;
 
 impl MovementAction for MoveToPreviousSibling {
 	fn get_target(_tree: &FsTreeView, selected_node: &NodeRef<FsTreeViewNode>) -> Option<NodeId> where Self: Sized {
-		selected_node.prev_sibling().map(|sibling| sibling.node_id())
+		selected_node.prev_sibling().or_else(|| selected_node.parent()).map(|node| node.node_id())
 	}
 }
