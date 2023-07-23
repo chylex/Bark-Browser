753898f6e0ad6ebbc111d66ab509ba1cb8a45589
chylex <contact@chylex.com>
2023-07-23 08:24:58 +0200

    Limit count to 99999 with an error message if exceeded

--- src/component/filesystem/action/count.rs
+++ src/component/filesystem/action/count.rs
@@ -1,3 +1,6 @@
+use ratatui::style::Color;
+
+use crate::component::dialog::message::{MessageDialogActionMap, MessageDialogLayer};
 use crate::component::filesystem::FsLayer;
 use crate::state::action::{Action, ActionResult};
 use crate::state::Environment;
@@ -9,8 +12,14 @@ impl Action<FsLayer> for PushCountDigit {
 		let next_digit = self.0 as usize;
 		
 		let old_count = layer.registers.count.unwrap_or(0);
-		layer.registers.count = Some(old_count * 10 + next_digit);
+		let new_count = old_count * 10 + next_digit;
 		
-		ActionResult::Nothing
+		if new_count > 99_999 {
+			layer.registers.count = None;
+			ActionResult::PushLayer(Box::new(MessageDialogLayer::new(Color::LightRed, "Error", "Count is too large (> 99999), it will be reset.", MessageDialogActionMap::ok())))
+		} else {
+			layer.registers.count = Some(new_count);
+			ActionResult::Nothing
+		}
 	}
 }
