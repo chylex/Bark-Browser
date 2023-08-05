19623d745a4d4276162a387fa2692e1907f071d9
chylex <contact@chylex.com>
2023-08-05 03:45:07 +0200

    Update wsl.sh to install cmake from default repositories

--- wsl.sh
+++ wsl.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y         \
      apt-transport-https        \
      build-essential            \
      ca-certificates            \
+     cmake                      \
      gdb                        \
      gnupg                      \
      software-properties-common \
@@ -15,9 +16,3 @@ sudo apt-get install -y         \
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 rustup component add rust-src
-
-# CMake
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-sudo apt-get update
-sudo apt-get install -y cmake
